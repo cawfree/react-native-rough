d451997f773247ce50aa022846d6d7850662c6df
Kelvin Chu <chubillkelvin@gmail.com>
2020-06-07 20:00:03 +0800

    Add Path component, run yarn prettify.

--- src/components/Arc.js
+++ src/components/Arc.js
@@ -1,32 +1,19 @@
 import React, { useCallback } from "react";
 import PropTypes from "prop-types";
-import {Path} from "react-native-svg";
-import {useRough} from "../hooks";
+import { Path } from "react-native-svg";
+import { useRough } from "../hooks";
 
 const Arc = ({ x, y, width, height, start, stop, closed, ...o }) => {
   const generator = useRough(o);
   const [a, b] = generator.toPaths(
-    generator.arc(
-      x,
-      y,
-      width,
-      height,
-      start,
-      stop,
-      closed,
-      o
-    ),
+    generator.arc(x, y, width, height, start, stop, closed, o)
   );
-  const {d: stroke, ...strokeProps} = (a || {});
-  const {d: fill, ...fillProps} = (b || {});
+  const { d: stroke, ...strokeProps } = a || {};
+  const { d: fill, ...fillProps } = b || {};
   return (
     <>
-      {(!!stroke) && (
-        <Path d={stroke} {...strokeProps}/>
-      )}
-      {(!!fill) && (
-        <Path d={fill} {...fillProps}/>
-      )}
+      {!!stroke && <Path d={stroke} {...strokeProps} />}
+      {!!fill && <Path d={fill} {...fillProps} />}
     </>
   );
 };
--- src/components/Line.js
+++ src/components/Line.js
@@ -1,29 +1,17 @@
 import React, { useCallback } from "react";
 import PropTypes from "prop-types";
-import {Path} from "react-native-svg";
-import {useRough} from "../hooks";
+import { Path } from "react-native-svg";
+import { useRough } from "../hooks";
 
 const Line = ({ x1, y1, x2, y2, ...o }) => {
   const generator = useRough(o);
-  const [a, b] = generator.toPaths(
-    generator.line(
-      x1,
-      y1,
-      x2,
-      y2,
-      o
-    )
-  );
-  const {d: stroke, ...strokeProps} = (a || {});
-  const {d: fill, ...fillProps} = (b || {});
+  const [a, b] = generator.toPaths(generator.line(x1, y1, x2, y2, o));
+  const { d: stroke, ...strokeProps } = a || {};
+  const { d: fill, ...fillProps } = b || {};
   return (
     <>
-      {(!!stroke) && (
-        <Path d={stroke} {...strokeProps}/>
-      )}
-      {(!!fill) && (
-        <Path d={fill} {...fillProps}/>
-      )}
+      {!!stroke && <Path d={stroke} {...strokeProps} />}
+      {!!fill && <Path d={fill} {...fillProps} />}
     </>
   );
 };
